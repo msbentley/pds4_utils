c6769b9172b7cb4cadac4b812c1b055e1e567451
Mark S. Bentley <mark@lunartech.org>
2021-05-17 11:48:14 +0200

    WIP: moving to GitHub actions CI/CD

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(name='pds4_utils',
-    version='0.2',
-    author='Mark S. Bentley',
-    author_email='mark@lunartech.org',
-    description='A collection of PDS4 utilities',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/msbentley/pds4_utils",
-    download_url = 'https://github.com/msbentley/pds4_utils/archive/0.1.tar.gz',
-    install_requires=['pandas','pyyaml','lxml','pds4-tools'],
-    python_requires='>=3.0',
-    keywords = ['PDS','archive','data'],
-    packages=['pds4_utils'],
-    zip_safe=False)
+if __name__ == "__main__":
+    setup()
