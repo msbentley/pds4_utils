00da3d6655d15b87d3ba9be3d22fdf3ea4002b34
Mark S. Bentley <mark.bentley@esa.int>
2022-10-21 15:24:20 +0200

    Changed logger instantiation

--- pds4_utils/__init__.py
+++ pds4_utils/__init__.py
@@ -9,5 +9,15 @@ __all__ = ['common', 'read', 'write', 'dbase']
 # Set up the nullhandler - user applications can configure how to
 # display log messages etc.
 import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+import sys
+# logging.getLogger(__name__).addHandler(logging.NullHandler())
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+logformat = format='%(levelname)s %(asctime)s (%(name)s): %(message)s'
+stream = logging.StreamHandler(sys.stdout)
+stream.setFormatter(logging.Formatter(logformat))
+stream.setLevel(logging.INFO)
+# stream.propagate = False
+log.addHandler(stream)
